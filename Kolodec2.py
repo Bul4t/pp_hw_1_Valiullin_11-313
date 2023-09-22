df29357354b3e3497318fe1c9946ba30854532e5
Bul4t <enot46161@gmail.com>
2023-09-22 16:27:26 +0300

    Added Kolodec.py

---
+++ Kolodec2.py
@@ -0,0 +1,7 @@
+num_g = 9.8
+num_time = input("Введите время в секундах: ")
+num_depth = (num_g * int(num_time) ** 2)/2
+print("Глубина колодца в m", num_depth)
+
+print("Нажмите enter!")
+a = input()
